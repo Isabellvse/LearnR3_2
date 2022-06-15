1ecb5ae4d861d389652a7a2da7a5de4028c554df
Isabell Victoria Strandby Ernst <isabell.ernst@hotmail.com>
2022-06-15 10:05:35 +0200

    added functions to import data

--- data-raw/mmash.R
+++ data-raw/mmash.R
@@ -1,4 +1,7 @@
 library(here)
+library(tidyverse)
+source(here("R/functions.R"))
+
 
 # define link to mmash data
 mmash_link <- "https://physionet.org/static/published-projects/mmash/multilevel-monitoring-of-activity-and-sleep-in-healthy-people-1.0.0.zip"
@@ -21,3 +24,14 @@ file_delete(here(c("data-raw/MMASH.zip",
                    "data-raw/SHA256SUMS.txt",
                    "data-raw/LICENSE.txt")))
 file_move(here("data-raw/DataPaper"), here("data-raw/mmash"))
+
+# Import functions
+user_info_df <- import_multiple_files("user_info.csv", import_user_info)
+saliva_df <- import_multiple_files("saliva.csv", import_saliva)
+rr_df <- import_multiple_files("RR.csv", import_rr)
+actigraph_df <- import_multiple_files("Actigraph.csv", import_actigraph)
+
+summarised_rr_df <- rr_df %>%
+    group_by(file_path_id, day) %>%
+    summarise(across(ibi_s, list(mean = mean, sd = sd), na.rm = TRUE)) %>%
+    ungroup()
